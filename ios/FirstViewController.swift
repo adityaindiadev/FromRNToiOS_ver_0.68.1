4804281ea43b323b7e6f05ddadb012562714751d
“adityaindiadev” <aditya.india.dev@gmail.com>
2023-08-28 16:37:03 +0530

    Yes, it is finally done, Now you can navigate to storyboard screen from react native. I just need to put RCT Root Screen into UINavigation.

---
+++ ios/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  FromRNToiOS
+//
+//  Created by Aditya Gupta on 25/08/23.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("voila")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
